53b57ea9e154a8c3f17b6c7971c861ddda27cbad
Gino Zhao <ginozhao2010@gmail.com>
2018-06-28 11:37:55 +0800

    add lock condition example

--- thread_safe/src/main/java/com/gino/thread/lock/LockCondition.java
+++ thread_safe/src/main/java/com/gino/thread/lock/LockCondition.java
@@ -3,12 +3,16 @@ package com.gino.thread.lock;
 /**
  * 面试题：写一个固定容量同步容器，拥有put和get方法，以及getCount方法，
  * 能够支持2个生产者线程以及10个消费者线程的阻塞调用
- *
+ * <p>
  * 使用Lock和Condition来实现
  * 对比两种方式，Condition的方式可以更加精确的指定哪些线程被唤醒
  *
- * @author mashibing
+ * @author gino
+ * Created on 2018/6/26
+ * @reference mashibing
  */
+
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
@@ -16,23 +20,25 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class MyContainer2<T> {
+@Slf4j
+public class LockCondition<T> {
     final private LinkedList<T> lists = new LinkedList<>();
-    final private int MAX = 10; //最多10个元素
+    final static private int NUM_CON = 10;
+    final static private int NUM_PRO = 2;
 
     private Lock lock = new ReentrantLock();
     private Condition producer = lock.newCondition();
     private Condition consumer = lock.newCondition();
 
-    public void put(T t) {
+    private void put(T t) {
         try {
             lock.lock();
-            while(lists.size() == MAX) { //想想为什么用while而不是用if？
-                producer.await();
+            while (lists.size() == NUM_CON) {
+                producer.await(); // release lock
             }
 
             lists.add(t);
-            consumer.signalAll(); //通知消费者线程进行消费
+            consumer.signalAll();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -40,15 +46,15 @@ public class MyContainer2<T> {
         }
     }
 
-    public T get() {
+    private T get() {
         T t = null;
         try {
             lock.lock();
-            while(lists.size() == 0) {
+            while (lists.size() == 0) {
                 consumer.await();
             }
             t = lists.removeFirst();
-            producer.signalAll(); //通知生产者进行生产
+            producer.signalAll();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -58,11 +64,13 @@ public class MyContainer2<T> {
     }
 
     public static void main(String[] args) {
-        MyContainer2<String> c = new MyContainer2<>();
-        //启动消费者线程
-        for(int i=0; i<10; i++) {
-            new Thread(()->{
-                for(int j=0; j<5; j++) System.out.println(Thread.currentThread().getName() + " " + c.get());
+        LockCondition<String> c = new LockCondition<>();
+        //start consumer threads
+        for (int i = 0; i < NUM_CON; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 5; j++) {
+                    log.info(c.get());
+                }
             }, "c" + i).start();
         }
 
@@ -72,10 +80,12 @@ public class MyContainer2<T> {
             e.printStackTrace();
         }
 
-        //启动生产者线程
-        for(int i=0; i<2; i++) {
-            new Thread(()->{
-                for(int j=0; j<25; j++) c.put(Thread.currentThread().getName() + " " + j);
+        //start producer threads
+        for (int i = 0; i < NUM_PRO; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 25; j++) {
+                    c.put(Thread.currentThread().getName() + " " + j);
+                }
             }, "p" + i).start();
         }
     }
