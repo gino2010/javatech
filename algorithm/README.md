c7052dcb191e77ff8a9ad8fb40c68473d5fbf9e1
Gino Zhao <ginozhao2010@gmail.com>
2018-09-22 16:20:30 +0800

    add bucket sort algorithm

---
+++ algorithm/README.md
@@ -0,0 +1,24 @@
+# algorithm
+
+算法，作为专业的程序员你会多少？
+
+Algorithm, how much would you master as a professional programmer?
+
+
+此工程每个文件将实现一个简单的、基础的算法，目的就是巩固学习一下，打实基础。
+
+Each file of this project will implement a simple, basic algorithm, the purpose is to consolidate and learn.
+
+算法也是一种思维方式，这种思维方式甚至可以延伸到很多非计算机领域中使用
+
+Algorithms are also a way of thinking, and this way of thinking can even be extended to many non-computer fields.
+
+# note
+由于最近项目的原因，没有时间做比较大的完整系统的实验，所以用算法这个工程练练手。
+
+Due to the recent project reasons, there is no time to do a relatively large complete application experiment, so use the algorithm to practice the hand.
+
+其实算法问题我还是喜欢python书写，所以时间允许的话我会在python实现一份，或者某些算法只在python里实现。
+
+In fact, I still like Python writing for algorithm problems, so I will implement one in Python if I have enough time, or some algorithms will only be implemented in Python.
+
---
+++ algorithm/src/main/java/com/gino/algorithm/BucketSort.java
@@ -0,0 +1,66 @@
+package com.gino.algorithm;
+
+import java.util.Scanner;
+
+/**
+ * Bucket Sort 桶排序
+ *
+ * @author gino
+ * Created on 2018/9/22
+ * <p>
+ * 这是一个非常好理解的排序算法，也很接近与现实人们的操作习惯。
+ * 处理效率很高，典型的空间换时间，时间复杂度为O(n)
+ * 什么时候用，最直观的感觉就是扑克牌类，手里的牌进行排序，你想想是不是这个道理。
+ * <p>
+ * 模拟，10以内的数字进行排序
+ * <p>
+ * for example: 1,2,9,1,5,7,6,3,4,8,8,2,3
+ */
+public class BucketSort {
+    private static int[] bucket = new int[11];
+    private static int[] data;
+
+    /**
+     * init bucket
+     */
+    private static void initBucket() {
+        for (int i = 0; i < bucket.length; i++) {
+            bucket[i] = 0;
+        }
+    }
+
+    /**
+     * sort
+     */
+    private static void sort() {
+        // put in bucket
+        for (int aData : data) {
+            bucket[aData]++;
+        }
+
+        // out of bucket
+        for (int i = 0; i < bucket.length; i++) {
+            for (int j = 0; j < bucket[i]; j++) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        initBucket();
+
+        // input data
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input your 0~9 number, split by comma: ");
+        String number = scanner.next();
+        String[] split = number.split(",");
+
+        // transfer data to numbers
+        data = new int[split.length];
+        for (int i = 0; i < split.length; i++) {
+            data[i] = Integer.valueOf(split[i]);
+        }
+
+        sort();
+    }
+}
