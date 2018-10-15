f304cec31665015259fa906cac485cbc8a88713e
Gino Zhao <ginozhao2010@gmail.com>
2018-10-15 15:55:01 +0800

    update kafka producer and consumer

--- mqtest/kafka_producer/src/main/java/com/mqtest/kafka/ProducerApplication.java
+++ mqtest/kafka_producer/src/main/java/com/mqtest/kafka/ProducerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @Slf4j
 @SpringBootApplication
@@ -19,6 +20,6 @@ public class ProducerApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("kafka producer server start...");
-        producer.send("gino test");
+        producer.send("kafka gino test");
     }
 }
