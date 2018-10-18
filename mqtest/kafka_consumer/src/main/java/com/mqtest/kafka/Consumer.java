41f34a47b039d6bb29f9fa3d0fc05f2245891826
Gino Zhao <ginozhao2010@gmail.com>
2018-10-18 14:19:23 +0800

    update kafka consumer for testing offset

--- mqtest/kafka_consumer/src/main/java/com/mqtest/kafka/Consumer.java
+++ mqtest/kafka_consumer/src/main/java/com/mqtest/kafka/Consumer.java
@@ -2,6 +2,7 @@ package com.mqtest.kafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
@@ -18,12 +19,13 @@ import java.util.List;
 @Component
 public class Consumer {
     @KafkaListener(topics = "${app.topic}")
-    public void receive(String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
+    public void receive(String message, Acknowledgment acknowledgment,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
                         @Header(KafkaHeaders.RECEIVED_TOPIC) List<String> topics,
                         @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
 
         // 模拟其中一个消费者处理时间较长
-        if (partitions.get(0)==0) {
+        if (partitions.get(0) == 0) {
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
@@ -32,5 +34,6 @@ public class Consumer {
         }
 
         log.info("receive message:{}, partition_id:{}, topic:{}, offsets:{}", message, partitions.get(0), topics.get(0), offsets.get(0));
+        acknowledgment.acknowledge();
     }
 }
--- mqtest/kafka_consumer/src/test/java/com/mqtest/kafka/ConsumerApplicationTests.java
+++ mqtest/kafka_consumer/src/test/java/com/mqtest/kafka/ConsumerApplicationTests.java
@@ -1,16 +1,47 @@
 package com.mqtest.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ConsumerApplicationTests {
+    @Autowired
+    ConsumerFactory consumerFactory;
 
     @Test
     public void contextLoads() {
+    }
+
+    /**
+     * 测试手动修改offset，制定消费起点
+     */
+    @Test
+    public void testOffset() {
+        TopicPartition topicPartition1 = new TopicPartition("gino.topic", 0);
+        KafkaConsumer consumer = (KafkaConsumer) consumerFactory.createConsumer();
+        consumer.assign(Collections.singletonList(topicPartition1));
+        consumer.seek(topicPartition1, 11);
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+                System.out.println();
+            }
+
+        }
     }
 
 }
