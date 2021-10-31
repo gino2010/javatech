0ab5c9325d43e0324d83295582981cea6e9a4cf7
gino <2063598+gino2010@users.noreply.github.com>
2021-10-31 14:44:56 +0800

    add some code

--- algorithm/src/main/java/com/gino/algorithm/RemoveDuplicates.java
+++ algorithm/src/main/java/com/gino/algorithm/RemoveDuplicates.java
@@ -1,20 +1,27 @@
 package com.gino.algorithm;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 public class RemoveDuplicates {
     public static int removeDuplicates(int[] nums) {
-        Set<Integer> integerSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
-        int[] ints = integerSet.stream().mapToInt(Integer::intValue).sorted().toArray();
-        for(int i=0; i<ints.length; i++){
-            nums[i] = ints[i];
+        if (nums.length == 0) {
+            return 0;
         }
-        return integerSet.size();
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        int size = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (result[size - 1] != nums[i]) {
+                result[size] = nums[i];
+                size++;
+            }
+        }
+        System.arraycopy(result, 0, nums, 0, size);
+        return size;
     }
 
     public static void main(String[] args) {
-        int[] data = new int[]{1, 2, 2, 3, 4};
+        int[] data = new int[]{1, 2, 2, 3, 4, 5, 5};
         System.out.println(removeDuplicates(data));
         Arrays.stream(data).forEach(System.out::print);
     }
